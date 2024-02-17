92e91a5d783c67f18c359ddc9212bdb858b098e1
Abhinay S. Bhadauria <knowledgepoint887@gmail.com>
2024-02-17 14:27:25 +0530

    Added basic RabitMQ config

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const { PORT } = require('./config/serverConfig');
 const apiRoutes = require('./routes/index');
 const db = require('./models/index');
 
-const setupAndServer = () => {
+const setupAndStartServer = () => {
 
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({ extended: true }));
@@ -15,12 +15,10 @@ const setupAndServer = () => {
 
     app.listen(PORT, () => {
         console.log(`Server started on port ${PORT}`);
-
         if (process.env.DB_SYNC) {
             db.sequelize.sync({ alter: true });
         }
-
     });
 }
 
-setupAndServer();
+setupAndStartServer();
